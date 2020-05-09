1073f3e609a89fc11017c6051e04df99a68f3020
Ezequiel <ezequieldeoliveiralima@gmail.com>
2020-05-09 12:18:58 -0300

    Embed HorizontalStackView in VerticalStackView

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Text("Target Color Block")
-            Text("Guess Color Block")
+        VStack {
+            HStack {
+                Text("Target Color Block")
+                Text("Guess Color Block")
+            }
         }
     }
 }
