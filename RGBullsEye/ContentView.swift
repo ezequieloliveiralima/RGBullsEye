6d864c3cfe755d58d152c1e15d1bdbb80fa27888
Ezequiel <ezequieldeoliveiralima@gmail.com>
2020-05-09 12:34:45 -0300

    Linking target color and guess color to respective rectangles

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -9,16 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+
     var body: some View {
         VStack {
             HStack {
                 VStack {
-                    Rectangle()
+                    Rectangle().foregroundColor(Color(red: rTarget, green: gTarget, blue: bTarget, opacity: 1.0))
                     Text("Match this color")
                 }
 
                 VStack {
-                    Rectangle()
+                    Rectangle().foregroundColor(Color(red: rGuess, green: gGuess, blue: bGuess, opacity: 1.0))
                     HStack {
                       Text("R: xxx")
                       Text("G: xxx")
@@ -40,6 +48,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
     }
 }
